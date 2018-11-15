224bf59d3a2a2fbd7114a8e867e2537e9a8ad6f8
Fergal Daly <fergal@esatclear.ie>
2018-11-15 14:31:46 -0800

    commit test the promise of suggest

--- test/fixed_strings_suggester_test.js
+++ test/fixed_strings_suggester_test.js
@@ -3,9 +3,16 @@ import {FixedStringsSuggester} from '../fixed_strings_suggester.js';
 var assert = require('assert');
 describe('FixedStringsSuggester', function() {
   describe('#suggestInternal', function() {
-    it('should TODO', function(){
+    it('should suggest "google" from "goo"', function(){
       var suggester = new FixedStringsSuggester(['gold', 'golf', 'google']);
       assert.deepEqual(["google"], suggester.suggestInternal('goo'));
+    });
+  });
+
+  describe('#suggest', function() {
+    it('should return a promise that resolves', async () => {
+      var suggester = new FixedStringsSuggester(['gold', 'golf', 'google']);
+      assert.deepEqual(["google"], await suggester.suggestInternal('goo'));
     });
   });
 });
